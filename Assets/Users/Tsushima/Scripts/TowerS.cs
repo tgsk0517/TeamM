923b03320abdc48ff46c0848fc752cf5f343fc2d
Kenshin <kenshin.tsushima@gmail.com>
2022-05-14 18:26:03 +0900

    タワーの発射間隔をインスペクター上で変更できるようになりました。

--- Assets/Users/Tsushima/Scripts/TowerS.cs
+++ Assets/Users/Tsushima/Scripts/TowerS.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class TowerS : TowerStats
 {
+    [SerializeField]
+    private float ATKctTime;
+    
     void Start()
     {
         _timer = 0.0f;
-        ATKctTime = 1.0f;
         ATKct = false;
     }
 
