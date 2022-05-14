945a4cb1beb65d119f9c682da5527301ad0bc739
小川 <21d3170005oy@edu.tech.ac.jp>
2022-05-14 18:28:35 +0900

    Inspector上でスポーン間隔を変更できるようにしておきました。

--- Assets/Users/ogawa/Scripts/RandomEnemySpawn.cs
+++ Assets/Users/ogawa/Scripts/RandomEnemySpawn.cs
@@ -6,10 +6,13 @@ public class RandomEnemySpawn : MonoBehaviour
 {
     public GameObject[] Enemy;
     private int number;
+    
+    public float SpawnInterval = 100.0f;
+
 
     void Update()
     {
-        if(Time.frameCount % 60 == 0)
+        if(Time.frameCount % SpawnInterval == 0)
         {
             float x = Random.Range(1920.0f, -1820.0f);
             float y = Random.Range(1080.0f, -1080.0f);
